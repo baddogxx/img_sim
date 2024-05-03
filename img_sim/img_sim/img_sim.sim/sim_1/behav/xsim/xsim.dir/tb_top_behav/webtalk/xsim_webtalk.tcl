9bdf304e5eb8d4fbf93d9288286dee2104cb2598
xu xiang <2521061290@qq.com>
2024-05-03 16:30:25 +0800

    图像处理ip怎么跑不起来了，这是怎么回事啊！

--- img_sim/img_sim/img_sim.sim/sim_1/behav/xsim/xsim.dir/tb_top_behav/webtalk/xsim_webtalk.tcl
+++ img_sim/img_sim/img_sim.sim/sim_1/behav/xsim/xsim.dir/tb_top_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir E:/git_repositories/img_sim/img_sim/img_sim/img_sim.sim/sim_1/behav/xsim/xsim.dir/tb_top_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Sat Apr 20 21:28:06 2024" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Thu May  2 21:34:49 2024" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2018.3 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2405991" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "427ab3b3-7b9d-4bca-be12-b414ee350697" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "264588a7b93b43a88adcb81fd475c51b" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "482" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "485" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Microsoft Windows 8 or later , 64-bit" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "12th Gen Intel(R) Core(TM) i7-12700" -context "user_environment"
@@ -25,8 +25,8 @@ webtalk_register_client -client xsim
 webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command_line_options"
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
 webtalk_add_data -client xsim -key runtime -value "18001 us" -context "xsim\\usage"
-webtalk_add_data -client xsim -key iteration -value "2" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "21.38_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "43736_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 1146362255 -regid "" -xml E:/git_repositories/img_sim/img_sim/img_sim/img_sim.sim/sim_1/behav/xsim/xsim.dir/tb_top_behav/webtalk/usage_statistics_ext_xsim.xml -html E:/git_repositories/img_sim/img_sim/img_sim/img_sim.sim/sim_1/behav/xsim/xsim.dir/tb_top_behav/webtalk/usage_statistics_ext_xsim.html -wdm E:/git_repositories/img_sim/img_sim/img_sim/img_sim.sim/sim_1/behav/xsim/xsim.dir/tb_top_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key iteration -value "3" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Time -value "21.06_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Memory -value "43048_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 1910470514 -regid "" -xml E:/git_repositories/img_sim/img_sim/img_sim/img_sim.sim/sim_1/behav/xsim/xsim.dir/tb_top_behav/webtalk/usage_statistics_ext_xsim.xml -html E:/git_repositories/img_sim/img_sim/img_sim/img_sim.sim/sim_1/behav/xsim/xsim.dir/tb_top_behav/webtalk/usage_statistics_ext_xsim.html -wdm E:/git_repositories/img_sim/img_sim/img_sim/img_sim.sim/sim_1/behav/xsim/xsim.dir/tb_top_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
